3cf94c61aaddd4f5700939fb30146e6c63c6faa0
liuhongze <lhz920301@163.com>
2019-09-23 14:02:41 +0800

    Enhance Read DateTime Module

--- src/2/reformatting_text_to_fixed_number_of_columns/example.py
+++ src/2/reformatting_text_to_fixed_number_of_columns/example.py
@@ -3,11 +3,12 @@
 # Examples of reformatting text to different column widths
 
 # A long string
+import textwrap
+
 s = "Look into my eyes, look into my eyes, the eyes, the eyes, \
 the eyes, not around the eyes, don't look around the eyes, \
 look into my eyes, you're under."
 
-import textwrap
 
 print(textwrap.fill(s, 70))
 print()
@@ -20,4 +21,3 @@ print()
 
 print(textwrap.fill(s, 40, subsequent_indent='    '))
 print()
-
